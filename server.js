81c4a09599eb6c11fdd20819606e2d6b38505731
Christopher Turton <christopheraturton@gmail.com>
2020-07-20 10:43:44 -0400

    html rearranging

--- server.js
+++ server.js
@@ -3,15 +3,16 @@ const express = require("express");
 const app = express();
 //Env Port or 8080
 let PORT = process.env.PORT || 8080;
-
+//Using handlebars
+var exphbs = require("express-handlebars");
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 //.env
 require("dotenv").config();
 //Moment
 const moment = require("moment");
 //Body parser
 const bodyParser = require('body-parser')
-//Bcrypt
-const bcrypt = require("bcryptjs");
 
 //Import db when built
 var db = require("./models");
@@ -25,15 +26,12 @@ app.use(express.static("public"));
 //Use sessions
 // const sessions = require("client-sessions");
 
-app.use(sessions({
-    cookieName: "session",
-    secret: process.env.cookieSecret,
-    duration: 20 * 60 * 1000 //20 minutes
-}))
-//Using handlebars
-var exphbs = require("express-handlebars");
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
+// app.use(sessions({
+//     cookieName: "session",
+//     secret: process.env.cookieSecret,
+//     duration: 20 * 60 * 1000 //20 minutes
+// }))
+
 //Import routes HERE
 require("./routes/html-routes")(app);
 require("./routes/watchlist-api-routes")(app);
