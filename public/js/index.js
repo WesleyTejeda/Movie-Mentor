e33c7a56566bd08a159f3fbe5f3381c9b31989ee
Christopher Turton <christopheraturton@gmail.com>
2020-07-20 13:49:11 -0400

    index handlebars

--- public/js/index.js
+++ public/js/index.js
@@ -1,68 +1,63 @@
 //Build out functionality
 $(document).ready(function () {
-  var loginForm = $("form.login");
-  var signUpForm = $("form.signup");
-  var usernameInput = $("input#loginUsername");
-  var passwordInput = $("input#loginPassword");
-  var registerName = "input#signUpUsername";
-  var registerPassword = "input#signUpPassword";
-
-  loginForm.on("submit", function (event) {
-    event.preventDefault();
-    var userData = {
-      username: usernameInput.val().trim(),
-      password: passwordInput.val().trim(),
-    };
-    if (!userData.username || !userData.password) {
-      return;
-    }
-
-    loginUser(userData.username, userData.password);
-    usernameInput.val("");
-    passwordInput.val("");
-  });
-
-  function loginUser(username, password) {
-    $.get("/api/login", {
-      username: username,
-      password: password,
-    })
-      .then(function () {
-        window.location.replace("/users");
-        // If there's an error, log the error
+    var loginForm = $(".login");
+    var signUpForm = $(".signup");
+    var usernameInput = $("#loginUsername");
+    var passwordInput = $("#loginPassword");
+    var registerName = $("#signUpUsername");
+    var registerPassword = $("#signUpPassword");
+    loginForm.on("submit", function (event) {
+      event.preventDefault();
+      var userData = {
+        username: usernameInput.val().trim(),
+        password: passwordInput.val().trim(),
+      };
+      if (!userData.username || !userData.password) {
+        return;
+      }
+      loginUser(userData.username, userData.password);
+      usernameInput.val("");
+      passwordInput.val("");
+    });
+    function loginUser(username, password) {
+      $.get("/api/login", {
+        username: username,
+        password: password,
       })
-      .catch(function (err) {
-        console.log(err);
-      });
-  }
-
-  signUpForm.on("submit", function (event) {
-    event.preventDefault();
-    var userData = {
-      username: registerName.val().trim(),
-      password: registerPassword.val().trim(),
-    };
-
-    if (!userData.username || !userData.password) {
-      return;
+        .then(function () {
+          window.location.replace("/users");
+          // If there's an error, log the error
+        })
+        .catch(function (err) {
+          console.log(err);
+        });
     }
-    registerUser(userData.username, userData.password);
-    usernameInput.val("");
-    passwordInput.val("");
-  });
-  function registerUser(username, password) {
-    $.post("/api/signup", {
-      username: username,
-      password: password,
-    })
-      .then(function (data) {
-        window.location.replace("/users");
+    signUpForm.on("submit", function (event) {
+      event.preventDefault();
+      var userData = {
+        username: registerName.val().trim(),
+        password: registerPassword.val().trim(),
+      };
+      console.log(userData);
+      if (!userData.username || !userData.password) {
+        return;
+      }
+      registerUser(userData.username, userData.password);
+      usernameInput.val("");
+      passwordInput.val("");
+    });
+    function registerUser(username, password) {
+      $.post("/api/signup", {
+        username: username,
+        password: password,
       })
-      .catch(handleLoginErr);
-  }
-
-  function handleLoginErr(err) {
-    $("#alert .msg").text(err.responseJSON);
-    $("#alert").fadeIn(500);
-  }
-});
+        .then(function (data) {
+          console.log("Signed up")
+        })
+        .catch(handleLoginErr);
+    }
+    function handleLoginErr(err) {
+      $("#alert .msg").text(err.responseJSON);
+      $("#alert").fadeIn(500);
+    }
+  });
--- server.js
+++ server.js
@@ -2,44 +2,20 @@
 const express = require("express");
 const app = express();
 let PORT = process.env.PORT || 8080;
-<<<<<<< HEAD
-//Using handlebars
-var exphbs = require("express-handlebars");
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
-//.env
-=======
->>>>>>> 2689bde6a5c77e558b3bff0dee616d4fc3af628c
 require("dotenv").config();
-
 const moment = require("moment");
 const bodyParser = require('body-parser')
-<<<<<<< HEAD
-=======
 const bcrypt = require("bcryptjs");
 const cookieParser = require("cookie-parser");
 app.use(cookieParser());
->>>>>>> 2689bde6a5c77e558b3bff0dee616d4fc3af628c
-
 //Import db when built
 var db = require("./models");
-
 //Use JSON and expect nested objects
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 //Static folder public
 app.use(express.static("public"));
-
 //Use sessions
-<<<<<<< HEAD
-// const sessions = require("client-sessions");
-
-// app.use(sessions({
-//     cookieName: "session",
-//     secret: process.env.cookieSecret,
-//     duration: 20 * 60 * 1000 //20 minutes
-// }))
-=======
 const sessions = require('client-sessions');
 app.use(sessions({
     cookieName: "session",
@@ -47,18 +23,14 @@ app.use(sessions({
     duration: 60 * 60 * 1000, // 60mins
     activeDuration: 5 * 60 * 1000 // +5mins if about to expire
 }));
-
 //Using handlebars
 var exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
->>>>>>> 2689bde6a5c77e558b3bff0dee616d4fc3af628c
-
 //Import routes HERE
 require("./routes/html-routes")(app);
 require("./routes/watchlist-api-routes")(app);
 require("./routes/user-api-routes")(app);
-
 //Listening on port
 db.sequelize.sync({force: true}).then(function() {
     app.listen(PORT, function() {
